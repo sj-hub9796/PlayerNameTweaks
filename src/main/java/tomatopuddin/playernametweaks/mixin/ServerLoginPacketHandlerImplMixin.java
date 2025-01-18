c89223f01cf9d64d73fbb6423f12d0b15a857226
sjhub <sj.hub9796@gmail.com>
2025-01-18 16:17:22 +0900

    bump to 1.21

--- src/main/java/tomatopuddin/playernametweaks/mixin/ServerLoginPacketHandlerImplMixin.java
+++ src/main/java/tomatopuddin/playernametweaks/mixin/ServerLoginPacketHandlerImplMixin.java
@@ -1,17 +1,17 @@
 package tomatopuddin.playernametweaks.mixin;
 
-import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import net.minecraft.util.StringUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(ServerLoginPacketListenerImpl.class)
+@Mixin(StringUtil.class)
 public class ServerLoginPacketHandlerImplMixin {
     /**
      * @author
      * @reason
      */
     @Overwrite
-    public static boolean isValidUsername(String p_203793_) {
+    public static boolean isValidPlayerName(String string) {
         return true;
     }
 }
