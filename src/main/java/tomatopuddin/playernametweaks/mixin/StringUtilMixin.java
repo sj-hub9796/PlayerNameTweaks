6c2363d7e2c64afa5058aeb08ab162aee3774406
sjhub <sj.hub9796@gmail.com>
2025-01-18 16:19:01 +0900

    Rename

--- src/main/java/tomatopuddin/playernametweaks/mixin/StringUtilMixin.java
+++ src/main/java/tomatopuddin/playernametweaks/mixin/StringUtilMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(StringUtil.class)
-public class ServerLoginPacketHandlerImplMixin {
+public class StringUtilMixin {
     /**
      * @author
      * @reason
